3d9d08aec42e696819ce789a8b2e91cbf818cd0b
Jothika <raviravi31799@gmail.com>
2026-06-11 08:25:14 +0530

    changed read_config

--- Pytestworkspace/POM/Utility/read_config.py
+++ Pytestworkspace/POM/Utility/read_config.py
@@ -1,6 +1,20 @@
 from configparser import ConfigParser
+import os
 
-def get_config(category,key):
-    config=ConfigParser()
-    config.read("./Configuration/config.ini")
-    return config.get(category,key)
+def get_config(category, key):
+    config = ConfigParser()
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(
+        os.path.dirname(current_dir),
+        "Configuration",
+        "config.ini"
+    )
+
+    print("Config Path:", config_path)
+
+    config.read(config_path)
+
+    print("Sections:", config.sections())
+
+    return config.get(category, key)
